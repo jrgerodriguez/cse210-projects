1ac32799b52557bdf8c8f4650abe4e3350c1a558
jrgerodriguez <menjivar.jorge.93@gmail.com>
2023-09-21 15:08:23 -0600

    Finished prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("This is in C#");
+
+        int number = 5;
+        number = 8;
+        number = number + 3;
     }
 }
