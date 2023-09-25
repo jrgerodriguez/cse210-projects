a68ee1c3516ffe7da026d79a42a2178549b49a7a
jrgerodriguez <menjivar.jorge.93@gmail.com>
2023-09-25 13:52:37 -0600

    Prep 3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        string playAgain = "yes";
+        
+        while (playAgain == "yes")
+        {
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+          
+        
+            int guessTimes = 0;
+            int yourGuess = -1;
+    
+            while (yourGuess != magicNumber)
+            {
+    
+                Console.Write("Guess the number: ");
+                yourGuess = int.Parse(Console.ReadLine());
+                guessTimes = guessTimes + 1;
+    
+                if (yourGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+        
+                else if (yourGuess < magicNumber)
+                {
+                    Console.WriteLine("Greater");
+                }
+        
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed it.");
+                }
+    
+            }
+    
+            Console.WriteLine($"You took {guessTimes} times to guess the number");
+            Console.WriteLine("Would you like to play again (yes/no)?");
+            playAgain = Console.ReadLine();
+        }
+
+
     }
 }
