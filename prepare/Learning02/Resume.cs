86c46b742e8421da8f9ebf3b5ef1e132706c8e86
jrgerodriguez <menjivar.jorge.93@gmail.com>
2023-10-02 20:15:49 -0600

    W03 Prepare: Learning Activity Complete

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Resume {
+
+    public string _name;
+    public List<Job> _jobs = new List<Job>();  // List<Job> will will be de data type
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Jobs:");
+        foreach (Job job in _jobs){
+            job.Display(); // Here I am calling the Display method I set up for Jobs.cs
+        }     
+    }
+
+
+    
+}
+
