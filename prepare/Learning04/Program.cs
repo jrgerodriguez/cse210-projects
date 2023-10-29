26a9ae5acae2e6f272b34f252781ec2e3c8a3b7e
jrgerodriguez <menjivar.jorge.93@gmail.com>
2023-10-29 13:01:33 -0600

    Inheritance Activity Complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions","7.3", "8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment write1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine();
+        Console.WriteLine(write1.GetSummary());
+        Console.WriteLine(write1.GetWritingInformation());
     }
 }
