8bb85874bdb5d2f6f4e8401b751c6a06e9a304a1
jrgerodriguez <menjivar.jorge.93@gmail.com>
2023-11-08 00:03:35 -0600

    Learning05 Activity Complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("blue", 5));
+        shapes.Add(new Rectangle("red", 10, 5));
+        shapes.Add(new Circle("yellow", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The area of the {color} shape is {area:N2} m");
+        }          
+
     }
 }
