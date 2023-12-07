2cfc381439667679749489ed3c994024dd5ff5f1
jrgerodriguez <menjivar.jorge.93@gmail.com>
2023-12-07 00:23:19 -0600

    Foundation 3 and 4 completed

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,22 @@
+public abstract class Activity
+{
+    protected string _date;
+    protected int _length;
+
+    public Activity(string date, int length)
+    {
+        _date = date;
+        _length = length;
+    }
+
+    public abstract double GetDistance();
+
+    public abstract double GetSpeed();
+
+    public abstract double GetPace();
+
+    public string GetSummary()
+    {
+        return $"{_date} {GetType()} ({_length} min)- Distance: {GetDistance():N2} km, Speed: {GetSpeed():N2} kph, Pace: {GetPace():N2} min per km.";
+    }
+}
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
+    static private List<Activity> activities = new List<Activity>();
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+
+        activities.Add(new Running("07 Dec 2023", 30, 4.8));
+        activities.Add(new Cycling("07 Dec 2023", 30, 8.5));
+        activities.Add(new Swimming("07 Dec 2023", 30, 100));
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
